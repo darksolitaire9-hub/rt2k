4954a48a0a8a410c257bd1210d62516e5e77fe2b
solitaire9 <darksolitaire9@gmail.com>
2026-06-04 16:57:27 +0100

    fix: update preset to cloudflare-pages and add wrangler.toml

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-01-01',
   nitro: {
-    preset: process.env.NITRO_PRESET || 'static',
+    preset: process.env.NITRO_PRESET || 'cloudflare-pages',
   },
   modules: ['@nuxt/ui', '@nuxt/test-utils/module', '@nuxtjs/seo'],
   site: {
